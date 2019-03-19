46a5fa44bf68e0f0ff3d0e6aa6bbeb237ddaba27
Nate <nate.m.luong@gmail.com>
2019-03-19 17:03:30 +0100

    work this time

---
+++ bret/static/bret/js/sparrow.js
@@ -0,0 +1,12 @@
+import 'babelPolyfill';
+import $ from 'jquery';
+
+<script src='https://lib.smartsparrow.com/simcapi-js-3.1.0.min.js'></script>
+
+let transporter = Transporter.getInstance();
+
+function init(args) {
+  console.log(JSON.stringify(args));
+
+  let studentID = transporter.getConfig().userData.id;
+}
