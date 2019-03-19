afd4d88b8246d2d1fff511e110b91d69fdbd6a63
Nate <nate.m.luong@gmail.com>
2019-03-19 18:39:21 +0100

    work!

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -1,14 +1,1 @@
-import Transporter from 'api/snapshot/Transporter';
-
-let transporter = Transporter.getInstance();
-
-$(document).ready(wait:function() {
-  transporter.addInitialSetupCompleteListener(init);
-  transporter.notifyOnReady();
-});
-
-function init(args) {
-  console.log(JSON.stringify(args));
-
-  let studentID = transporter.getConfig().userData.id;
-};
+simcapi.Transporter.addInitialSetupCompleteListener(function(configObject) { console.log(JSON.stringify(configObject); });
