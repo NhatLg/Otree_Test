db49bafe23e5192ce562651dec158b397ba70a9f
Nate <nate.m.luong@gmail.com>
2019-03-19 18:50:36 +0100

    yes!

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -1,1 +1,1 @@
-simcapi.Transporter.addInitialSetupCompleteListener(function(configObject) { console.log(JSON.stringify(configObject)); });
+simcapi.Transporter.addInitialSetupCompleteListener(function(userData) { console.log(JSON.stringify(userData)); });
