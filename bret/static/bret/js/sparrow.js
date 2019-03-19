603e8e1a6a1e35e3b68323997dac207d3706b91e
Nate <nate.m.luong@gmail.com>
2019-03-19 17:20:04 +0100

    work please

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -1,6 +1,14 @@
+import 'babelPolyfill';
+import $ from 'jquery';
+
 <script src='https://lib.smartsparrow.com/simcapi-js-3.1.0.min.js'></script>
 
 let transporter = Transporter.getInstance();
+
+$(document).ready(wait:function() {
+  transporter.addInitialSetupCompleteListener(init);
+  transporter.notifyOnReady();
+});
 
 function init(args) {
   console.log(JSON.stringify(args));
