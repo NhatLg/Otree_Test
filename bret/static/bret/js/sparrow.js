6247c5d3d3ad4179129a18773a6d6781cd66678f
Nate <nate.m.luong@gmail.com>
2019-03-20 19:12:23 +0100

    help

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -1,15 +1,3 @@
 console.log(5+5);
 
-var simModel = new simcapi.CapiAdapter.CapiModel({
-    studentId: '5',
-    });
-
-simcapi.CapiAdapter.expose('studentId', simModel,
-                                            {readonly: true});
-
-simcapi.Transporter.notifyOnReady();
-
-simModel.set('studentId', simcapi.Transporter.getConfig());
-
-var valueId = simModel.get('studentId');
-console.log(valueId);
+console.log(JSON.stringify(configObject);
