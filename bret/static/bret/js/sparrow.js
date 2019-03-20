6fef3acda16fc8903dafe03d59b4d57d3abd4314
Nate <nate.m.luong@gmail.com>
2019-03-20 18:15:30 +0100

    work pls

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -1,3 +1,4 @@
 var studentID = simcapi.Transporter.getConfig();
 console.log(5+5);
 console.log(studentID);
+simcapi.Transporter.addInitialSetupCompleteListener(function(configObject) { console.log(JSON.stringify(configObject)); });
