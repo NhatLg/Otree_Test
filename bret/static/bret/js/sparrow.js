4fb4a17a5c9fa87928dd4e280442b8c1c408d48f
Nate <nate.m.luong@gmail.com>
2019-03-21 16:59:30 +0100

    pushing forward

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -3,4 +3,5 @@ console.log(5+5);
 simcapi.Transporter.addInitialSetupCompleteListener(function(configObject) { console.log(JSON.stringify(configObject)); });
 
 var subjectId = simcapi.Transporter.getConfig();
+console.log(JSON.stringify(args))
 console.log(subjectId);
