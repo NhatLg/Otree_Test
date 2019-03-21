a4e103316dc273d9a809ff627ece903353fd52f8
Nate <nate.m.luong@gmail.com>
2019-03-21 17:11:32 +0100

    push 2

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -3,7 +3,7 @@ console.log(5+5);
 simcapi.Transporter.addInitialSetupCompleteListener(function(configObject) { console.log(JSON.stringify(configObject)); });
 
 var subjectId = simcapi.Transporter.getConfig();
-console.log(JSON.stringify(args));
+
 console.log(subjectId);
 
 let transporter = Transporter.getInstance();
