49eff815c7dae0d80715b1f4fc654789ea28fef1
Nate <nate.m.luong@gmail.com>
2019-03-29 17:05:17 +0100

    work

--- bret/static/bret/js/sparrow.js
+++ bret/static/bret/js/sparrow.js
@@ -3,7 +3,9 @@ console.log(5+5);
 simcapi.Transporter.addInitialSetupCompleteListener(init);
 
 function init(args) {
-  console.log(JSON.stringify(args));
+  console.log(window.$log = JSON.stringify(args));
 };
 
 simcapi.Transporter.notifyOnReady();
+
+console.log($log.handshake.config.userData.id);
